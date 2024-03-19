a22e751e5494c75d8003b3237f2963874ad382d9
sivab <sivab@backbase.com>
2024-03-20 00:21:09 +0530

    feat: add module `mssc-beer-service`

---
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.repositories;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-17.
+ */
+@Repository
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
