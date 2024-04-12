b63d364f11793f14ab149583eb1c6399944012f5
sivab <sivab@backbase.com>
2024-04-12 11:35:50 +0530

    implementing New Inventory Listener - closes #8

--- mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package guru.sfg.beer.inventory.service.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +11,13 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 @RequiredArgsConstructor
 public class JmsConfig {
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
     @Bean
-    public MessageConverter messageConverter() {
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
         messageConverter.setTargetType(MessageType.TEXT);
         messageConverter.setTypeIdPropertyName("_type");
+        messageConverter.setObjectMapper(objectMapper);
         return messageConverter;
     }
 }
