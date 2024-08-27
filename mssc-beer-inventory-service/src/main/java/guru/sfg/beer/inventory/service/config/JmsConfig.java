9d1b9d6689ceb003cfb6fd3c5e821bf94a75130d
bsd <datta.bathina15@gmail.com>
2024-08-27 15:28:38 +0530

    adding deallocation consumer. closes #16

--- mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -14,6 +14,8 @@ public class JmsConfig {
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
     public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
     public static final String ALLOCATE_ORDER_RESULT = "allocate-order-result";
+    public static final String DEALLOCATE_ORDER_QUEUE = "deallocate-order";
+
     @Bean
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
--- mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
+++ mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
@@ -9,4 +9,5 @@ import guru.sfg.brewery.model.BeerOrderDto;
 public interface AllocationService {
 
     Boolean allocateOrder(BeerOrderDto beerOrderDto);
+    void deallocateOrder(BeerOrderDto beerOrderDto);
 }
