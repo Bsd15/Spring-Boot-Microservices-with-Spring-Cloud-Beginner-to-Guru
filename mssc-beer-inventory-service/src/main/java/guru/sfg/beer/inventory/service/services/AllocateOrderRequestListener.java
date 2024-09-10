6062ce94507352f9e024714ed8df2e90035542c6
bsd <datta.bathina15@gmail.com>
2024-09-10 13:27:38 +0530

    Fixing missing property - closes #17

--- mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/services/AllocateOrderRequestListener.java
+++ mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/services/AllocateOrderRequestListener.java
@@ -17,21 +17,27 @@ public class AllocateOrderRequestListener {
     private final AllocationService allocationService;
 
     @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
-    public void listen(AllocateOrderRequest allocateOrderRequest) {
+    public void listen(AllocateOrderRequest request) {
         log.debug("Inside AllocateOrderRequestListener::listen()");
-        AllocateOrderResult.AllocateOrderResultBuilder allocateOrderResultBuilder = AllocateOrderResult.builder();
-        allocateOrderResultBuilder.beerOrderDto(allocateOrderRequest.getBeerOrderDto());
-        boolean allocationError;
-        try {
-            allocationError = allocationService.allocateOrder(allocateOrderRequest.getBeerOrderDto());
-        } catch (Exception e) {
-            log.error("Allocation failed for Order Id {}", allocateOrderRequest.getBeerOrderDto().getId(), e);
-            allocationError = false;
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+        builder.beerOrderDto(request.getBeerOrderDto());
+
+        try{
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+            if (allocationResult){
+                builder.pendingInventory(false);
+            } else {
+                builder.pendingInventory(true);
+            }
+
+            builder.allocationError(false);
+        } catch (Exception e){
+            log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
+            builder.allocationError(true);
         }
 
-        allocateOrderResultBuilder.allocationError(allocationError);
-        allocateOrderResultBuilder.pendingInventory(!allocationError);
-
-        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESULT, allocateOrderResultBuilder.build());
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESULT,
+                builder.build());
     }
 }
