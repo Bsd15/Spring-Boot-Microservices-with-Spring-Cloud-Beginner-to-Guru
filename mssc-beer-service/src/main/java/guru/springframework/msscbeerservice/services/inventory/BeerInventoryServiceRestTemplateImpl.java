3dd1da68cf7defc8535dfd3acd727f41b04ec0f9
bsd <datta.bathina15@gmail.com>
2024-09-22 15:38:01 +0530

    configuring OpenFeign Client. Closes #55

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.msscbeerservice.services.inventory.model.BeerInvento
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,10 @@ import java.util.UUID;
  */
 @Slf4j
 @Component
+@Profile("!local-discovery")
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService {
+
+    public static final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
 
     private final String inventoryPath;
     private final RestTemplate restTemplate;
