d4adceb8f00ccee24c2429f3b2c3e27b732f3c26
bsd <datta.bathina15@gmail.com>
2024-09-24 17:41:21 +0530

    config res4j for gateway circuit breaker. closes #7

--- mssc-brewery-gateway/src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ mssc-brewery-gateway/src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -13,12 +13,20 @@ public class LoadBalancedRoutesConfig {
     public RouteLocator loadBalancedRoutes(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder
                 .routes()
-                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                .route("beer-service", r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("lb://beer-service"))
-                .route(r -> r.path("/api/v1/customers*", "/api/v1/customers/*")
+                .route("order-servicer", r -> r.path("/api/v1/customers*", "/api/v1/customers/*")
                         .uri("lb://order-service"))
-                .route(r -> r.path("/api/v1/beer/*/inventory")
+                .route("inventory-service", r -> r.path("/api/v1/beer/*/inventory")
+                        .filters(f -> f.circuitBreaker(
+                                        c -> c.setName("inventoryCB")
+                                                .setFallbackUri("forward:/inventory-failover")
+                                                .setRouteId("inv-failover")
+                                )
+                        )
                         .uri("lb://inventory-service"))
+                .route("inventory-failover-service", r -> r.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover"))
                 .build();
     }
 }
